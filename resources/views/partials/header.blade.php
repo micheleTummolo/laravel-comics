bc6d9e2281e1a6563d457a6bb49c8c46f426c9ac
micheleTummolo <tummolomichele02@gmail.com>
2023-02-16 19:10:39 +0100

    Aded header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,37 @@
-<div class="container">
-    <h1>Header</h1>
+<div class="header_container">
+    <div class="logo_container">
+        <img src="{{ Vite::asset('resources/img/dc-logo.png')}}" alt="">
+    </div>
+
+    <nav>
+        <ul class="m-0">
+            <li>
+                <a href="#">Caracters</a>
+            </li>
+            <li>
+                <a href="#" class="active">Comics</a>
+            </li>
+            <li>
+                <a href="#">Movies</a>
+            </li>
+            <li>
+                <a href="#">Tv</a> 
+            </li>
+            <li>
+                <a href="#">Games</a>  
+            </li>
+            <li>
+                <a href="#">Collectives</a>  
+            </li>
+            <li>
+                <a href="#">Videos</a>  
+            </li>
+            <li>
+                <a href="#">Fans</a>  
+            </li>
+            <li>
+                <a href="#">News</a>  
+            </li>
+        </ul>
+    </nav>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     $comics = config('db');
-    dd($comics);
-    
+
     return view('index');
 });
