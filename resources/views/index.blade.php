551a8d4b176051edc7878d08bb92da60b92bd218
micheleTummolo <tummolomichele02@gmail.com>
2023-02-16 22:51:01 +0100

    Done cards section

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -19,9 +19,26 @@
     <div class="jumbotron">
         <img src="{{ Vite::asset('resources/img/jumbotron.jpg')}}" alt="">
     </div>
-    <div class="container">
-        <h1>Comics</h1>
-    </div>
+    <main>
+        <div class="main_container">
+            <div class="current_series_label">
+                <h2 class="m-0">CURRENT SERIES</h2>
+            </div>
+            <div class="card_container mt-3">
+                @foreach($comics as $comic)
+                <div class="my_card">
+                    <div class="thumb">
+                        <img src="{{ $comic['thumb']}}" alt="">
+                    </div>
+                    <h4>{{ $comic['title']}}</h4>
+                </div>
+                @endforeach
+            </div>
+            <div class="button_container">
+                <button><a href="#">LOAD MORE</a></button>
+            </div>
+        </div>
+    </main>
 </body>
 <footer>
     @include('partials.footer')
