62704d1da5079fcc42487d7ef772796d9b3b6baf
micheleTummolo <tummolomichele02@gmail.com>
2023-02-16 18:14:34 +0100

    First commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>DC Comics - comics</title>
+
+    <!-- Styles -->
+    @vite('resources/js/app.js')
+
+</head>
+
+<header>
+    @include('partials.header')
+</header>
+<body>
+    <div class="container">
+        <h1>Comics</h1>
+    </div>
+</body>
+<footer>
+    @include('partials.footer')
+</footer>
